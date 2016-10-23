49ba6eccf8b044fa5d1c5a78a777f45f1a43289b
Yvan <yvan.zy@gmail.com>
2016-10-23 15:29:29 +0800

    新增猜数字游戏作业

---
+++ 周次(1)使用入门/Guess num.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# *coding: utf-8
+import random
+
+def play_game(num):
+    while True:
+        a = int(raw_input("Please input a number: "))
+        if a > num:
+            print "The number is bigger"
+        elif a < num:
+            print "The number is smaller"
+        else :
+            print "You are right!"
+            break
+
+def main():
+    print "--------begin game---------"
+    num = random.randint(0,99)
+    play_game(num)
+
+if __name__ ==  '__main__':
+    main()
